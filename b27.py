75f36682b86267a2ecd4ca06c21c92312b578fb8
ShwetaMadhavan <46585864+ShwetaMadhavan@users.noreply.github.com>
2019-01-16 18:12:01 +0530

    Create b27.py

---
+++ b27.py
@@ -0,0 +1,11 @@
+x=int(input())
+y=int(input())
+for i in range(x+1,y):
+    c=0
+    for j in range(2,i):
+        if(i%j==0):
+            c=c+1
+            break
+    if(c==0):
+        print(i,end=" ")
+        
