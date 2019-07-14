41388ea89949bbd87242667c034877e94be0f010
ShwetaMadhavan <46585864+ShwetaMadhavan@users.noreply.github.com>
2019-07-14 22:24:06 +0530

    Create 39.java

---
+++ 39.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int h;
+h=m/60;
+m=m%60;
+System.out.println(h+" "+m);
+}
+}
