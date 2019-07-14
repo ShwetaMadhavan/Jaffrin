061af93c004f0f6426b8a9d86c15d52a261b32b4
ShwetaMadhavan <46585864+ShwetaMadhavan@users.noreply.github.com>
2019-07-14 21:46:31 +0530

    Create 31.java

---
+++ 31.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a=sc.nextInt();
+int d=sc.nextInt();
+sum=(n/2)*((2*a)+((n-1)*d))
+System.out.println(sum);
+}
+}
