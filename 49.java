371cb9e333cfdea20f5d91b5b65b4b4e4fe3eb27
ShwetaMadhavan <46585864+ShwetaMadhavan@users.noreply.github.com>
2019-07-15 21:55:22 +0530

    Update 49.java

--- 49.java
+++ 49.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution{
 public static void main(String[] args){
-Scanner sc=new Scanner(System.in)
+Scanner sc=new Scanner(System.in);
 int a[]=new int[10];
 int temp;
 for(int i=0;i<10;i++){
