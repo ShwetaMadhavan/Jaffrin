7bf1def21fe46f7d172443973d14a74b6f9d1a94
ShwetaMadhavan <46585864+ShwetaMadhavan@users.noreply.github.com>
2019-07-15 21:36:31 +0530

    Create 48.java

---
+++ 48.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in)
+a=sc.nextInt();
+b=sc.nextInt();
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a+" "+b);
+}
+}
